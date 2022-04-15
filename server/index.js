8531ca0351ff1f1a59c2f1793517f729ca5f7982
Bhrigu <joshi.bhrigu25@gmail.com>
2022-04-15 00:21:22 -0400

    'changes made for connecting to SDC'

--- server/index.js
+++ server/index.js
@@ -5,17 +5,45 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
 const port = 3000;
-const { API_URL, API_KEY } = require('./config.js');
+const { API_URL, API_KEY, SDC_URL } = require('./config.js');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.all('*', (req, res, next) => {
+  // res.header('Access-Control-Allow-Origin", "http://localhost:3000');
+  // res.header('Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE');
+  // res.header('Content-Type: application/json');
+  next();
+});
+
 app.all('/*', (req, res) => {
 
-  const apiPaths = ['/products', '/reviews', '/qa', '/cart', '/interactions'];
+  const apiPaths = ['/products', '/reviews', '/cart', '/interactions'];
   let isApiPath = apiPaths.some(apiPath => req.url.startsWith(apiPath));
+
+  if (req.url.startsWith('/qa')) {
+    axios({
+      headers: { 'Authorization': API_KEY },
+      baseURL: SDC_URL,
+      url: req.url,
+      method: req.method,
+      data: req.body
+    })
+      .then((apiResponse) => {
+        console.log('inside the API response', apiResponse);
+        res.send({
+          data: apiResponse.data,
+          status: apiResponse.status
+        });
+      })
+      .catch((err) => {
+        console.log('error in questions', err);
+        res.status(err.response.status).send('API request error.');
+      });
+  }
 
   if (isApiPath) {
     axios({
@@ -49,7 +77,7 @@ app.all('/*', (req, res) => {
       })
       .catch((err) => {
         console.log(err);
-        res.redirect('/');
+        // res.redirect('/');
       });
   }
 });
